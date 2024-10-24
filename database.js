a4d7acc5ff4528c7399d1a84cad8b6460bce086f
viictor1 <victorgaspalmeida@hotmail.com>
2024-10-23 22:25:57 -0300

    mudanças infra

--- database.js
+++ database.js
@@ -9,4 +9,27 @@ const pool = new Pool({
   port: process.env.PORT,
 });
 
+const createTable = async () => {
+    const createTableQuery = `
+        CREATE TABLE IF NOT EXISTS produto (
+          id SERIAL PRIMARY KEY,
+          descricao VARCHAR(200) NOT NULL,
+          preco FLOAT NOT NULL,
+          quantidade INTEGER NOT NULL,
+          data DATETIME DEFAULT CURRENT_DATETIME
+      );
+    `;
+  
+    try {
+      await pool.query(createTableQuery);
+      console.log("Tabela 'produtos' criada com sucesso.");
+    } catch (error) {
+      console.error("Erro criando tabela:", error);
+    } finally {
+      await pool.end();
+    }
+  };
+
+createTable();
+
 module.exports = pool;
